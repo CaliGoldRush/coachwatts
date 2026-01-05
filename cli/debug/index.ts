450ceb12678d50b87571531108f3551ab222da4c
Laszlo Racz <lracz@newpush.com>
2026-01-05 20:04:38 +0100

    feat(cli): add debug workout command for data troubleshooting

--- cli/debug/index.ts
+++ cli/debug/index.ts
@@ -1,10 +1,12 @@
 
 import { Command } from 'commander';
 import authLogicCommand from './auth-logic';
+import troubleshootWorkoutsCommand from './workout';
 
 const debugCommand = new Command('debug')
     .description('Debug commands');
 
 debugCommand.addCommand(authLogicCommand);
+debugCommand.addCommand(troubleshootWorkoutsCommand);
 
 export default debugCommand;
