4588a8907fad31af61ddfc835e0cce2f4a2b01a3
Laszlo Racz <lracz@newpush.com>
2026-01-16 12:51:43 +0100

    feat(cli): add redis command for connection check

--- cli/check/index.ts
+++ cli/check/index.ts
@@ -2,11 +2,13 @@ import { Command } from 'commander'
 import dbSchemaCommand from './db-schema'
 import gpsCommand from './gps'
 import buildCommand from './build'
+import redisCommand from './redis'
 
 const checkCommand = new Command('check').description('Check commands')
 
 checkCommand.addCommand(dbSchemaCommand)
 checkCommand.addCommand(gpsCommand)
 checkCommand.addCommand(buildCommand)
+checkCommand.addCommand(redisCommand)
 
 export default checkCommand
